20ef53a9796e244e0a24e5004e9f24288402e1b1
FreezeYourDiary <freezeyourdiary@gmail.com>
2025-03-05 17:42:44 +0100

    Add project files.

---
+++ Lab1/Lab1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FizzBuzz numbers = new FizzBuzz();
+            numbers.PrintFizzBuzz();
+        }
+    }
+
+    class FizzBuzz {
+        int maxNumber;
+        string fizzbuzz;
+        public FizzBuzz()
+        {
+            int maxNumber = Convert.ToInt32(Console.ReadLine());
+            this.maxNumber = maxNumber;
+        }
+        public void PrintFizzBuzz()
+        {
+            for (int i = 1; i< maxNumber; i++)
+            {    
+                fizzbuzz = (i % 3 == 0 && i % 5 == 0) ? "FizzBuzz" :
+                (i % 3 == 0) ? "Fizz" :
+                (i % 5 == 0) ? "Buzz" :
+                i.ToString();
+
+                Console.WriteLine(fizzbuzz);
+            }
+            
+        }
+    }
+
+}
